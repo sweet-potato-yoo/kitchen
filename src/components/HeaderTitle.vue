0580ef0522d21d8ef54d8c7222b46f1a095f885c
sweet-potato-yoo <yooaina98@gmail.com>
2024-09-01 00:27:11 +0900

    feat: main header title

---
+++ src/components/HeaderTitle.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="header-title">
+        <div class="header-title-text">Set Out <br /> Life Beautiful <br /> Through Cooking.</div>
+        <button class="header-title-button">Shop Out Collection</button>
+    </div>
+</template>
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+    
+})
+</script>
+<style lang="scss" scoped>
+    .header-title {
+        max-width: 690px;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        row-gap: 40px;
+    }
+    .header-title-text {
+        font-size: 80px;
+        text-align: left;
+        font-weight: 600;
+        color: #000000;
+    }
+    .header-title-button {
+        max-width: 280px;
+        width: 100%;
+        height: 80px;
+        background-color: #C2B8B2;
+        color: #000000;
+        border: 1px solid #A59D9A;
+        font-size: 16px;
+        font-weight: 600;
+    }
+</style>
--- src/components/MainHeadComponent.vue
+++ src/components/MainHeadComponent.vue
@@ -1,20 +1,27 @@
 <template>
     <div class="head">
-        
+        <HeaderTitle />
     </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
+import HeaderTitle from './HeaderTitle.vue';
 export default defineComponent({
-    
+    components: {
+        HeaderTitle
+    }
 })
 </script>
 <style lang="scss" scoped>
     .head {
-        max-width: 1800px;
-        width: 100%;
+        max-width: 1624px;
+        width: calc(100% - 176px);
         height: calc(100vh - 120px);
         border: 4px solid #A59D9A;
         border-top: none; 
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        padding: 0px 88px;
     }
 </style>
